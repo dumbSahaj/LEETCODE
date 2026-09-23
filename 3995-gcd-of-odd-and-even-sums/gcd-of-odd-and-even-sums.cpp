4ed474b77aeb8f0797dff263ab937c37e8fbaae7
dumbSahaj <sahajsaxena5659@gmail.com>
2026-09-24 01:54:12 +0530

    Time: 574 ms (5.09%) | Memory: 8.4 MB (81.69%) - LeetSync

--- 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.cpp
+++ 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.cpp
@@ -7,9 +7,10 @@ public:
             sumOdd += (2*i+1);
             sumEven += (2*i+2);
         }
-        for (int i = 1; i <= min(sumOdd,sumEven); i++){
+        for (int i = min(sumOdd,sumEven); i >= 1; i--){
             if ( (sumOdd % i) == 0 && (sumEven % i ) == 0){
                 gcd = i;
+                break;
             }
         }
         return gcd;
