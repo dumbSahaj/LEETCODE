ec8d5de202a1a19dfb4e591fbc49f6c05ebad4e3
dumbSahaj <sahajsaxena5659@gmail.com>
2026-09-26 01:13:15 +0530

    Time: 13 ms (31.51%) | Memory: 7.8 MB (37.56%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,8 +1,9 @@
 class Solution {
 public:
     int fib(int n) {
-        double rootfive = sqrt(5);
-        int nth = (pow((1+rootfive)/2,n)-pow((1-rootfive)/2,n))/rootfive;
-        return nth;
+        if (n<=1){
+            return n;
+        }
+        return fib(n-1)+fib(n-2);
     }
 };
